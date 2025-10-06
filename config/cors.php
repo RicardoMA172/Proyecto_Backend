95601059953856b6ede2cf5efb61c15ee149ee04
RicardoMA172 <98186789+RicardoMA172@users.noreply.github.com>
2025-10-05 21:31:17 -0600

    Añadir origen permitido para CORS en la configuración

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,12 @@
 return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:4200', 'http://127.0.0.1:4200'],
+
+    'allowed_origins' => [
+        'http://localhost:4200', 
+        'http://127.0.0.1:4200',
+        'https://boisterous-cajeta-e9a57b.netlify.app',],
+    
     'allowed_headers' => ['*'],
     'supports_credentials' => false,
 ];
