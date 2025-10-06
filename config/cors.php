d4603747404f6c8821c3694d7030823cd8a3a421
RicardoMA172 <98186789+RicardoMA172@users.noreply.github.com>
2025-10-05 21:38:21 -0600

    Corregir formato de la lista de orígenes permitidos en la configuración de CORS

--- config/cors.php
+++ config/cors.php
@@ -7,7 +7,7 @@ return [
     'allowed_origins' => [
         'http://localhost:4200', 
         'http://127.0.0.1:4200',
-        'https://boisterous-cajeta-e9a57b.netlify.app',],
+        'https://boisterous-cajeta-e9a57b.netlify.app'],
     
     'allowed_headers' => ['*'],
     // Allow cookies/credentials from the frontend (required for Sanctum SPA auth)
