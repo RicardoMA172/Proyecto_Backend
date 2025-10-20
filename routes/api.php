800557ebb1dc433b50376a75c6a2382cde5fe6c6
RicardoMA172 <98186789+RicardoMA172@users.noreply.github.com>
2025-10-20 11:13:51 -0600

    Agregar nuevos endpoints para obtener registros y exportar datos en la API

--- routes/api.php
+++ routes/api.php
@@ -6,8 +6,9 @@ use App\Http\Controllers\CalidadAireController;
 
 
 //http://localhost:8000/api/device/data
+// Endpoint para recibir datos del dispositivo
 Route::post('/device/data', [CalidadAireController::class, 'storeDeviceData']);
-
+// Endpoint para el dashboard de calidad del aire
 Route::get('/dashboard', [CalidadAireController::class, 'dashboard']);
 
 Route::get('/device/co', [CalidadAireController::class, 'coBetween']);
@@ -19,7 +20,9 @@ Route::get('/device/pm25', [CalidadAireController::class, 'pm25Between']);
 
 //NUEVO: Endpoints para obtener los últimos registros o todos los registros
 Route::get('/device/latest', [CalidadAireController::class, 'latest']);
+//NUEVO: Endpoint para obtener todos los registros
 Route::get('/device/all', [CalidadAireController::class, 'allRecords']);
+//NUEVO: Endpoint para obtener registros desde una fecha específica
 Route::get('/device/since', [CalidadAireController::class,'since']);
 
 
@@ -27,7 +30,7 @@ Route::get('/device/since', [CalidadAireController::class,'since']);
 Route::get('/device/by-date', [CalidadAireController::class, 'ByDate']);
 // NUEVO: Obtener los últimos registros de un día específico
 Route::get('/device/latest-by-date', [CalidadAireController::class, 'LatestByDate']);
-
+// Exportar datos a CSV
 Route::get('/device/export-csv', [App\Http\Controllers\CalidadAireController::class, 'exportCsv']);
 
 
